491fe97625bdf496057bcb84a7399c01f4685051
adancaym <adancaym@hotmail.com>
2023-11-21 13:49:18 -0600

    add readme

---
+++ readme.js
@@ -0,0 +1,22 @@
+"use strict";
+clone;
+the;
+repo;
+git;
+clone;
+https: //github.com/adancaym/scrapers
+ go;
+to;
+the;
+folder;
+cd;
+scrapers;
+install;
+the;
+dependencies;
+yarn;
+run;
+the;
+script;
+yarn;
+start;
---
+++ readme.md
@@ -0,0 +1,12 @@
+clone the repo
+
+git clone https://github.com/adancaym/scrapers
+
+go to the folder
+cd scrapers
+
+install the dependencies
+yarn 
+
+run the script
+yarn start
